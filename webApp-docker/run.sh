b6bbed7345968b0034cfab584f38ab08077f1686
EC2 Default User <ec2-user@ip-172-31-87-183.ec2.internal>
2019-11-19 15:36:25 +0000

    Modify Dockerfile for Jenkins job

--- webApp-docker/run.sh
+++
@@ -1,3 +0,0 @@
-docker run -d -p 8080:8080 -p 2222:22 --name my-docker-container my-docker-image
-sleep 5s
-curl localhost:8080
--- webApp-docker/stop-clean.sh
+++
@@ -1,3 +0,0 @@
-docker stop my-docker-container
-docker rm my-docker-container
-docker rmi my-docker-image
